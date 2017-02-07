6955f0172fa3cccf46e921900bc023abb5e9c44f
tran.quoc.quan <tran.quoc.quan@softfront.com.vn>
2017-02-07 15:52:23 +0700

    quan

---
+++ SideMenuController/SideMenuController.h
@@ -0,0 +1,19 @@
+//
+//  SideMenuController.h
+//  SideMenuController
+//
+//  Created by tran.quoc.quan on 2/7/17.
+//  Copyright © 2017 tran.quoc.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideMenuController.
+FOUNDATION_EXPORT double SideMenuControllerVersionNumber;
+
+//! Project version string for SideMenuController.
+FOUNDATION_EXPORT const unsigned char SideMenuControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideMenuController/PublicHeader.h>
+
+
